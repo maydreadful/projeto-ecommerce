d8d885f9d2529a65a5f52f0aa90ae43a81208ed6
VictorSampaioDev <joaovictorsampaio.ti@gmail.com>
2026-03-11 09:02:08 -0300

    Update

--- src/pages/Auth/Login/LoginForm.jsx
+++ src/pages/Auth/Login/LoginForm.jsx
@@ -46,7 +46,7 @@ const LoginForm = () => {
         }
     }
     return (
-        <div className=" min-h-screen  flex flex-col">
+        <div className=" min-h-screen  bg-zinc-800 flex flex-col">
             <main className="flex-1 flex items-center justify-center px-4">
                 <div className="w-full max-w-md bg-white rounded-2xl shadow-md p-6 sm:p-8">
                     <h2 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
